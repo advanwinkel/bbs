fd158d01f06ef597e1e4cbc0bd139f196706e0f4
Ad van Winkel <advanwinkel@gmail.com>
2015-11-01 13:51:59 +0100

    Fully working wuth socketio

--- assets/js/controllers/homePageController.js
+++ assets/js/controllers/homePageController.js
@@ -21,46 +21,36 @@ var vm = new Vue({
   	// We dedicate a method to retrieving and setting some data
 	  fetchEvents: function() {
 
-			var evts = [];
-
-			io.socket.get('/event', function whenServerResponds(resp, JWR) {
+				io.socket.get('/event', function whenServerResponds(resp, JWR) {
         
         console.log('Fetched videos and subscribed... Response:', resp);
-        
-        //console.log(resp.length);
-        
-        for(var i=0;i<resp.length;i++){
-				     evts.push(resp[i]);
-				}
-        //this.$set('events', evts);
+     
         if (JWR.statusCode >= 400) {
    
           console.log('something bad happened',resp);
     
           return;
         }
+        // $set is a convenience method provided by Vue that is similar to pushing
+	    	// data onto an array
+        vm.$set('events', resp);
       });
-
-	    // $set is a convenience method provided by Vue that is similar to pushing
-	    // data onto an array
-	    console.log("evts: " + evts);
-	    this.$set('events', evts);
     
-	    io.socket.on('event', function whenAVideoIsCreatedUpdatedOrDestroyed(msg) {
+	    io.socket.on('event', function whenAnEventIsCreatedUpdatedOrDestroyed(msg) {
         console.log('Is it firing',msg);
         if (msg.verb == "created") {
-		      vm.events.unshift({
+		      vm.events.push({
 			          name: msg.data.name,
 			          description: msg.data.description,
-			          date: msg.data.date
+			          date: msg.data.date,
+			          id: msg.id
 	      	 });
 	    	}
 	    	if (msg.verb == "destroyed"){
-	        vm.events.unshift({
-			          name: msg.previous.name,
-			          description: msg.previous.description,
-			          date: msg.previous.date
-      	 	});
+	        var arr = vm.events.filter(function dontdelete(obj){
+	        	return obj.id != msg.id 
+	       	});
+	       	vm.events = arr;
 	    	}
       });
      },
@@ -71,46 +61,38 @@ var vm = new Vue({
 	     	var evt = this.event;
 				//console.log(evt);
 
-				io.socket.post('/event',
-					{name: evt.name,
-					 description: evt.description,
-					 date: evt.date },
-					 function whenServerResponds(resp, JWR) {
-					//console.log(JWR.statusCode);
-					//console.log(JWR);
+				io.socket.post('/event', vm.event,
+					function whenServerResponds(resp, JWR) {
+
 					if (JWR.statusCode != 201) {
    
 	          console.log('something bad happened',resp);
 	    
 	          return;
         	}
-					evt.id = resp.id;
+        	vm.event.id = resp.id;
+					vm.events.push(vm.event);
+					vm.event = { name: '', description: '', date: '' }
 				});
-				this.events.push(this.event);
-				this.event = { name: '', description: '', date: '' }
-				
 	    }
-
-	    
-
 	  },
 
 	  deleteEvent: function(index) {
 	  
 		  if(confirm("Are you sure you want to delete this event?")) {
 
-				var id_delete = this.events[index].id;
+		  	var id_delete = vm.events[index].id;
 				io.socket.delete('/event/' + id_delete, function (response, JWR) {
 					//console.log(JWR.statusCode);
 					if (JWR.statusCode != 200) {
    
-	          console.log('something bad happened',resp);
+	          console.log('something bad happened',response);
 	    
 	          return;
         	}
+        	vm.events.$remove(index);
 				  console.log("event " + id_delete + " deleted");
 				});
-				this.events.$remove(index);	
 		  }
 		}
   }
